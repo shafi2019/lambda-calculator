ba424353c3b20eb5c47d13c22f5f4057963d9f90
Shafi Masoumi <masoumi2017@icloud.com>
2019-09-24 16:46:48 -0700

    Special Buttons Good

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -8,11 +8,15 @@ import { specials } from "../../../data";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [spe] = useState(specials);
   return (
     <div className="specials">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {spe.map((el, index) => (
+        <SpecialButton key={index} specialData={el} />
+      ))}
     </div>
   );
 };
